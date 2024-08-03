6f502300e26dcbd80b5d62a86c1b1c6fb41c6cda
amarthya <amarthyaseshu2@gmail.com>
2024-08-03 20:13:04 +0530

    2st commit

--- src/main/java/com/amarthya/spring_boot_concepts/SpringBootConceptsApplication.java
+++ src/main/java/com/amarthya/spring_boot_concepts/SpringBootConceptsApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootConceptsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootConceptsApplication.class, args);
+		System.out.println("hi");
+
 	}
 
 }
