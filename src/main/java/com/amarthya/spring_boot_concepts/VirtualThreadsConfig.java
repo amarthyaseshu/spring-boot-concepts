ae8078fcc70941d2eba17009e7dc2a8c970cf462
amarthya <amarthyaseshu2@gmail.com>
2024-09-08 16:10:26 +0530

    commit

---
+++ src/main/java/com/amarthya/spring_boot_concepts/VirtualThreadsConfig.java
@@ -0,0 +1,16 @@
+package com.amarthya.spring_boot_concepts;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VirtualThreadsConfig {
+
+    public TomcatProtocolHandlerCustomizer tomcatProtocolHandlerCustomizer(){
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Excecutors.new VirtualThreadPerTaskExecutor());
+        };
+    }
+}
+
+
